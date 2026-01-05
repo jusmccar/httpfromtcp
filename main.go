7cba46d8399552599b44501f0e168a8e64eba7bb
jusmccar <justinmccartney1@yahoo.com>
2026-01-05 13:13:50 -0800

    Add file byte reader

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buffer)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Println("Error reading file:", err)
+
+			return
+		}
+
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
 }
