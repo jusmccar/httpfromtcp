898feeb07ac15a0dba5d9f573bb26c4b3902ac39
jusmccar <justinmccartney1@yahoo.com>
2026-01-05 14:24:12 -0800

    Add getLinesChannel function

--- main.go
+++ main.go
@@ -3,45 +3,59 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		log.Fatal("Error opening file:", err)
 	}
 
-	defer file.Close()
+	lines := getLinesChannel(f)
 
-	buffer := make([]byte, 8)
-	currLine := ""
-
-	for {
-		n, err := file.Read(buffer)
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			fmt.Println("Error reading file:", err)
-			return
-		}
-
-		text := string(buffer[:n])
-		lines := strings.Split(text, "\n")
-
-		for i, line := range lines {
-			currLine += line
-
-			if i < len(lines)-1 {
-				fmt.Printf("read: %s\n", currLine)
-				currLine = ""
-			}
-		}
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+
+				log.Fatal("Error reading file:", err)
+			}
+
+			text := string(data[:n])
+			lines := strings.Split(text, "\n")
+
+			for i, line := range lines {
+				str += line
+
+				if i < len(lines)-1 {
+					out <- str
+					str = ""
+				}
+			}
+		}
+	}()
+
+	return out
 }
