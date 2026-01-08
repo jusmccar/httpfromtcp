0298ce6cd3b35fba8f015bbc3eb19a9fdcd3c565
jusmccar <justinmccartney1@yahoo.com>
2026-01-07 20:57:32 -0800

    Print headers

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -32,5 +32,10 @@ func main() {
 		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
 		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+		fmt.Printf("Headers:\n")
+
+		for key, value := range req.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
 	}
 }
